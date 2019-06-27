8c3e2ce9f3b6c58664c95fdf74b955a9f889132e
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-06-27 10:15:45 +0300

    new example module is ready

--- example/src/main/java/com/kaspersky/test_server/MainActivity.kt
+++ example/src/main/java/com/kaspersky/test_server/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.kaspersky.test_server
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private val executor = Executors.newSingleThreadExecutor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnExecuteAdb = findViewById<Button>(R.id.btn_execute_adb)
+        val editTextAdb = findViewById<EditText>(R.id.et_adb_command)
+        btnExecuteAdb.setOnClickListener { onBtnClick(editTextAdb) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        AdbConnection.start()
+    }
+
+    override fun onPause() {
+        AdbConnection.stop()
+        super.onPause()
+    }
+
+    private fun onBtnClick(editTextAdb: EditText) {
+        val command = editTextAdb.text.toString()
+        if (command.isNotEmpty()) {
+            executor.execute {
+                val result = kotlin.runCatching { AdbConnection.execute(command) }
+                runOnUiThread {
+                    Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
