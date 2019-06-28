06c64e8a0bcd4f3b267ae86a7227979c9a73e7b5
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-06-28 14:13:54 +0300

    serializing of CommandResult model

--- common/src/main/java/com/kaspersky/test_server/api/CommandResult.kt
+++ common/src/main/java/com/kaspersky/test_server/api/CommandResult.kt
@@ -1,9 +1,11 @@
 package com.kaspersky.test_server.api
 
+import java.io.Serializable
+
 data class CommandResult(
     val status: ExecutorResultStatus,
     val description: String
-)
+) : Serializable
 
 enum class ExecutorResultStatus {
     SUCCESS,
