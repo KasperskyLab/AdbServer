1f386b593ecc01032efddc6f9ef83fec16cb1f52
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-08-20 13:32:02 +0300

    more safe algorithm to create a client port (simple random) + create separate serverSocket for each desktop-client connection

--- desktop_device_connection/src/main/java/com/kaspersky/test_server/DesktopDeviceSocketConnectionForwardImpl.kt
+++ desktop_device_connection/src/main/java/com/kaspersky/test_server/DesktopDeviceSocketConnectionForwardImpl.kt
@@ -4,7 +4,7 @@ import com.kaspersky.test_server.api.CommandExecutor
 import com.kaspresky.test_server.log.Logger
 import java.net.ServerSocket
 import java.net.Socket
-import java.util.concurrent.atomic.AtomicInteger
+import kotlin.random.Random
 
 internal class DesktopDeviceSocketConnectionForwardImpl(
     private val logger: Logger
@@ -12,12 +12,13 @@ internal class DesktopDeviceSocketConnectionForwardImpl(
 
     companion object {
         private const val DEVICE_PORT: Int = 8500
+        private const val MIN_CLIENT_PORT: Int = 6000
+        private const val MAX_CLIENT_PORT: Int = 49000
         private const val LOCAL_HOST: String = "127.0.0.1"
-        private val LAST_CLIENT_PORT = AtomicInteger(9000)
     }
 
     private val tag = javaClass.simpleName
-    private val serverSocket by lazy { ServerSocket(DEVICE_PORT) }
+    private val clientPortsList: MutableList<Int> = mutableListOf()
 
     override fun getDesktopSocketLoad(executor: CommandExecutor): () -> Socket {
         val clientPort = getFreePort()
@@ -33,7 +34,15 @@ internal class DesktopDeviceSocketConnectionForwardImpl(
     }
 
     private fun getFreePort(): Int {
-        return LAST_CLIENT_PORT.incrementAndGet()
+        var newClientPort: Int
+        while (true) {
+            newClientPort = Random.Default.nextInt(MIN_CLIENT_PORT, MAX_CLIENT_PORT)
+            if (!clientPortsList.contains(newClientPort)) {
+                break
+            }
+            clientPortsList.add(newClientPort)
+        }
+        return newClientPort
     }
 
     private fun forwardPorts(executor: CommandExecutor, fromPort: Int, toPort: Int) {
@@ -44,6 +53,7 @@ internal class DesktopDeviceSocketConnectionForwardImpl(
 
     override fun getDeviceSocketLoad(): () -> Socket = {
         logger.i(tag, "getDeviceSocketLoad", "started")
+        val serverSocket by lazy { ServerSocket(DEVICE_PORT) }
         val readyServerSocket = serverSocket.accept()
         logger.i(tag, "getDeviceSocketLoad", "completed")
         readyServerSocket
