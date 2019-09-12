d899bfc0f0399e4e25da2a21551e88c2b3d953aa
matsyuk <Evgeny.Matsyuk@kaspersky.com>
2019-09-12 17:27:55 +0300

    androidX is introduced

--- example/src/main/java/com/kaspersky/test_server/MainActivity.kt
+++ example/src/main/java/com/kaspersky/test_server/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.kaspersky.test_server
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
